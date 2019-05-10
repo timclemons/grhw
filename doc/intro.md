b054b675e595c057c6d19d196018c648f0d80824
Tim Clemons <tclemons@gmail.com>
2019-05-10 12:51:20 -0700

    initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to grhw
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/grhw/core_test.clj
@@ -0,0 +1,7 @@
+(ns grhw.core-test
+  (:require [clojure.test :refer :all]
+            [grhw.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
